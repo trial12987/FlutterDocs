9238b9d447fb562368a89c666c9069da78739862
trial12987 <trial12987@gmail.com>
2022-10-11 14:38:45 +0300

    2

---
+++ flutter_docs/lib/gesture_detector.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: SampleApp()));
+
+class SampleApp extends StatefulWidget {
+  const SampleApp({super.key});
+
+  @override
+  State<SampleApp> createState() => _SampleAppState();
+}
+
+class _SampleAppState extends State<SampleApp>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late CurvedAnimation curve;
+  double imageSize = 100;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2000),
+    );
+    curve = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeIn,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Listener(
+          onPointerSignal: (pointerSignal) {
+            if (pointerSignal is PointerScrollEvent) {
+              Offset offset = pointerSignal.scrollDelta;
+              var imgSize = imageSize;
+
+              if (offset.direction > 0) {
+                if (imgSize < 400) imgSize = imgSize + 20;
+              } else {
+                if (imgSize > 40) imgSize = imgSize - 20;
+              }
+              setState(() {
+                imageSize = imgSize;
+              });
+            }
+          },
+          child: GestureDetector(
+            onDoubleTap: () {
+              if (controller.isCompleted) {
+                controller.reverse();
+              } else {
+                controller.forward();
+              }
+            },
+            child: RotationTransition(
+              turns: curve,
+              child: FlutterLogo(
+                size: imageSize,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_docs/lib/layout.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  const SampleApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  const SampleAppPage({super.key});
+
+  @override
+  State<SampleAppPage> createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  // Default value for toggle.
+  bool toggle = true;
+  void _toggle() {
+    setState(() {
+      toggle = !toggle;
+    });
+  }
+
+  Widget _getToggleChild() {
+    if (toggle) {
+      return const Text('Toggle One');
+    } else {
+      return ElevatedButton(
+        onPressed: () {},
+        child: const Text('Toggle Two'),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample App'),
+      ),
+      body: Center(
+        child: _getToggleChild(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggle,
+        tooltip: 'Update Text',
+        child: const Icon(Icons.update),
+      ),
+    );
+  }
+}
